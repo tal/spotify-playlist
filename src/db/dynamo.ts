4c6032392c0098afbe1b2f53af8c570040ceec0a
Tal Atlas <me@tal.by>
2019-02-18 22:55:01 -0500

    prefix all writtes with username

---
+++ src/db/dynamo.ts
@@ -0,0 +1,104 @@
+import { AWS } from '../aws'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+
+export class Dynamo {
+  constructor(public readonly user: UserData) {}
+
+  gId(suffix: string) {
+    return `${this.user.id}:${suffix}`
+  }
+
+  async getActionHistory(id: string, since: number) {
+    const docs = await AWS.docs
+    const resp = await docs
+      .query({
+        TableName: 'action_history',
+        KeyConditionExpression: 'id = :id AND created_at > :limit',
+        ExpressionAttributeValues: {
+          ':id': this.gId(id),
+          ':limit': since,
+        },
+        Limit: 1,
+      })
+      .promise()
+
+    if (resp.Items) {
+      return resp.Items[0] as ActionHistoryItemData | undefined
+    } else {
+      return undefined
+    }
+  }
+
+  async updateAccessToken(id: string, token: string, expiresAt: number) {
+    var params: DocumentClient.UpdateItemInput = {
+      TableName: 'user',
+      Key: { id },
+      UpdateExpression:
+        'set spotifyAuth.accessToken = :at, spotifyAuth.expiresAt = :exp',
+      ExpressionAttributeValues: {
+        ':at': token,
+        ':exp': expiresAt,
+      },
+      ReturnValues: 'ALL_NEW',
+    }
+
+    const docs = await AWS.docs
+    const response = await docs.update(params).promise()
+
+    if (response.Attributes) {
+      return response.Attributes as UserData
+    } else {
+      throw 'no data returned from update for some reason'
+    }
+  }
+
+  async putActionHistory(history: ActionHistoryItemData) {
+    const docs = await AWS.docs
+
+    const params = {
+      TableName: 'action_history',
+      Item: { ...history, id: this.gId(history.id) },
+    }
+
+    await docs.put(params).promise()
+
+    return history
+  }
+
+  async putUser(user: UserData) {
+    var params = {
+      TableName: 'user',
+      Item: {
+        id: user.id,
+        spotifyAuth: {
+          refreshToken: user.spotifyAuth.refreshToken,
+          accessToken: user.spotifyAuth.accessToken,
+          expiresAt: user.spotifyAuth.expiresAt,
+        },
+      },
+    }
+    const docs = await AWS.docs
+    await docs.put(params).promise()
+
+    return user
+  }
+}
+
+async function getUser(userName: string) {
+  const docs = await AWS.docs
+  const resp = await docs
+    .get({ TableName: 'user', Key: { id: userName } })
+    .promise()
+
+  return resp.Item as UserData | undefined
+}
+
+export async function getDynamo(userName?: string) {
+  if (!userName) return
+
+  const user = await getUser(userName)
+
+  if (!user) return
+
+  return new Dynamo(user)
+}
