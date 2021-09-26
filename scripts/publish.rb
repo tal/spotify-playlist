5c1161a8d06dbd71f483494b0138e7500b69e000
Tal Atlas <me@tal.by>
2021-09-26 14:23:56 -0400

    have it do all the things every 6 hours

--- scripts/publish.rb
+++ scripts/publish.rb
@@ -5,3 +5,5 @@ end
 system(%q{zip -r index.zip . -x".env" -x".git/*" -x"dynamodb_local_latest/*" -x"node_modules/typescript/*"})
 
 system("aws lambda update-function-code --function-name spotify-playlist-dev --zip-file fileb://index.zip")
+
+File.delete('index.zip')
